190fd1672162e34baa14c81e16a62161501f211d
germovaa <germo.vaa@gmail.com>
2023-10-05 17:35:22 +0300

    first commit

---
+++ yl3.py
@@ -0,0 +1,10 @@
+#Kirjuta programm, mis küsib kasutajalt täisarvu n vahemikus 1-9. 
+#Arvuta n + nn + nnn väärtus ja väljasta see. 
+#(Näiteks kui kasutaja sisestab 2, siis on vaja väljastada tulemus 2 + 	22 + 222 = 246). 
+#Ära kasuta korrutamistehet. Ülesanne on lahendatav ainult liitmise operaatorit kasuades.
+
+n = input("Sisesta täisarv n vahemikus 1-9: ")
+nn = n + n
+nnn = n + n + n
+value = int(n) + int(nn) + int(nnn)
+print(n + "+" + nn, "+", nnn, "=", value)
